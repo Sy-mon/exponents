22f2dc9917843435068b84cdceaa57e923e0a854
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2024-09-15 11:25:38 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+//Given two number 8 to the power of 12. Write code that could solve this as an integer...
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int a =  (int) Math.pow(8, 12); //This converts the double answer into an integer
+
+        System.out.println(a);
+
+    }
+}
